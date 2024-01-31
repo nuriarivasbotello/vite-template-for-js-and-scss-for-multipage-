79d76b4aafa3db4dd66c882327d9c993c78ce65e
nuriarivasbotello <nuriarivasbotello@gmail.com>
2024-01-31 18:45:00 +0100

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,13 +1,9 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
-import catImage from '../assets/images/cat.jpeg';
-import '../scss/styles.scss';
 
-const img = document.createElement('img');
-const button = document.getElementById('button');
-img.src = catImage;
+// import '../scss/styles.scss';
 
-document.body.append(img);
+// const button = document.getElementById('button');
 
-button.addEventListener('click', () => {
-  console.log('BUTTON CLICKED');
-});
+// button.addEventListener('click', () => {
+//   console.log('BUTTON CLICKED');
+// });
